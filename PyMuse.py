7fde0beb1a3f8ccef79b1cc2bd004adc5d203b5a
= <krpouncy0@gmail.com>
2019-10-03 14:01:39 -0600

    fixed progam end problem

--- PyMuse.py
+++ PyMuse.py
@@ -3,6 +3,15 @@ import argparse
 import os
 import sys
 import time
+import logging
+
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
+logging.basicConfig(filename="pymuse.log",
+                    level=logging.INFO,
+                    format=LOG_FORMAT,
+                    filemode='w')
+
+logger = logging.getLogger()
 
 
 class Headband:
@@ -20,37 +29,47 @@ class Headband:
         # brainwave eeg
         self.brainwave_raw = []
 
-        # variable to allow server shutdown
-        self.running = False
-
         # create the server
         self.ip = ip
         self.port = port
         self.server = self.create_server(self.ip, self.port)
 
+    def setServerInfo(self, ip, port):
+        try:
+            if isinstance(ip, str) and isinstance(port, int):
+                # update the values and create server
+                self.ip = ip
+                self.port = port
+                self.server = self.create_server(self.ip, self.port)
+                # log the information
+                logging.info("Server IP set to " + ip)
+                logging.info("Server Port set to " + str(port))
+        except BaseException:
+            logging.error("setServerInfo: ip and/or port given is not valid")
+            print("ERROR: IP and/or Port is not valid")
+            sys.exit(1)
+
     def start_server(self):
         """ starts an osc server """
-        print("server ip:", self.ip)
-        print("server port:", self.port)
-        print("awaiting headset connection")
+        print("[server ip:", self.ip, "] [server port:", self.port, "]")
+        print("awaiting headband connection\n")
         # start the progam
-        self.running = True
-        if self.running:
-            try:
-                self.server.serve_forever()
-            except KeyboardInterrupt:
-                self.running = False
-        else:
+        try:
+            logging.info("server started")
+            self.server.serve_forever()
+        except KeyboardInterrupt:
+            logging.debug("start_server: Program ended - KeyboardInterrupt")
             sys.exit(0)
 
     def stop(self):
-        self.running = False
+        logging.info("server stopped")
         self.server.shutdown()
 
     def run(self):
-        """ a method that updates at 256hz """
-        print(self.get_brainwaves())
-        # print(self.get_raw_brainwaves())
+        """ a method updates at 10hz """
+        if self.check_connection():
+            print(self.get_brainwaves())
+            # print(self.get_raw_brainwaves())
 
     def exit_handler(self):
         print("Program End")
@@ -65,20 +84,36 @@ class Headband:
         self.connection_status = [TP9, Fp1, Fp2, TP10]
 
     def check_connection(self):
-        # if connection is good return true, else return false
-        if all([self.connection_status[i] < 3 for i in range(4)]):
-            # reset connection notification
-            self.connection_notified = False
+        # ignore inital connection_status of [4,4,4,4]
+        if type(self.connection_status[0]) is type(4):
             return True
-        else:
-            # if the user hasn't been notified, then do so
-            if not self.connection_notified:
-                self.connection_notified = True
-                os.system('cls')
-                print("connection error:", self.connection_status)
-            return False
+
+        try:
+            # if connection is good return true, else return false
+            if all([self.connection_status[i] < 3 for i in range(4)]):
+                # reset connection notification
+                self.connection_notified = False
+                return True
+            else:
+                # if the user hasn't been notified, then do so
+                if not self.connection_notified:
+                    logging.info("poor Connection - " +
+                                 str(self.connection_status))
+                    self.connection_notified = True
+                    os.system('cls')
+                    print("connection error:", self.connection_status)
+                return False
+        except KeyboardInterrupt:
+            logging.debug(
+                "check_connection: Program ended - KeyboardInterrupt")
+            sys.exit(0)
 
     """ handlers organized from slowest to fastest """
+
+    def brainwave_raw_handler(self, addr, *args):
+        if self.check_connection():
+            self.brainwave_raw = self.round_values(args[1:])
+
     def theta_abs_handler(self, addr, args, TP9, Fp1, Fp2, TP10):
         if self.check_connection():
             self.brainwave_theta = self.round_values([TP9, Fp1, Fp2, TP10])
@@ -98,29 +133,25 @@ class Headband:
     def gamma_abs_handler(self, addr, args, TP9, Fp1, Fp2, TP10):
         if self.check_connection():
             self.brainwave_gamma = self.round_values([TP9, Fp1, Fp2, TP10])
-
-    def brainwave_raw_handler(self, addr, *args):
-        if self.check_connection():
-            self.brainwave_raw = self.round_values(args[1:])
-            # start the progam
-            if self.running:
-                try:
-                    self.run()
-                except KeyboardInterrupt:
-                    self.running = False
-            else:
-                sys.exit(0)
+        # start the progam
+        logging.debug("calling run function from 'gamma_abs_handler'")
+        self.run()
 
     def get_brainwaves(self, rounded=3):
         # values are updated at 10hz
         # create a dictionary
-        brainwaves_levels = {
+        brainwaves_dict = {
             "alpha": self.brainwave_alpha,
             "theta": self.brainwave_theta,
             "delta": self.brainwave_delta,
             "beta": self.brainwave_beta,
             "gamma": self.brainwave_gamma}
-        return brainwaves_levels
+
+        # check if any lists in the dict are empty
+        if [] in brainwaves_dict.values():
+            return {}
+        else:
+            return brainwaves_dict
 
     def get_raw_brainwaves(self, rounded=3):
         # values are updated at 256hz
@@ -171,8 +202,9 @@ class Headband:
         # create the server
         return osc_server.ThreadingOSCUDPServer((args.ip, args.port), dp)
 
+
 if __name__ == "__main__":
     ip = input("Enter Computer IP Address: ")
     print("default port set to 5000")
-    headband = Headband(ip = ip)
+    headband = Headband(ip=ip)
     headband.start_server()
